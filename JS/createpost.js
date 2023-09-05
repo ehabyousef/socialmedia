ecf27976baed4fa74f232e74f6ae406204aeefe3
‪Ehab Yousef‬‏ <ehabyousef022@gmail.com>
2023-09-05 23:21:21 +0300

    add autho

--- JS/createpost.js
+++ JS/createpost.js
@@ -2,14 +2,19 @@ let newPost = {
   body: "",
   image: "",
 };
-
+const token = localStorage.getItem("token");
+let headers = {
+  authorization: `Bearer ${token}`,
+};
 function handleCretePost(e) {
   newPost[e.name] = e.value;
   console.log(newPost);
 }
 function createPost() {
   axios
-    .post(`https://tarmeezacademy.com/api/v1/posts`)
+    .post(`https://tarmeezacademy.com/api/v1/posts`, {
+      headers: headers,
+    })
     .then((Response) => console.log(Response))
     .catch((err) => console.log(err));
 }

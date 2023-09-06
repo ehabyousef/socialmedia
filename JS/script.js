03d7a55ecaa840c9fed380bec8ae2e86a6085146
‪Ehab Yousef‬‏ <ehabyousef022@gmail.com>
2023-09-06 23:48:30 +0300

    add value

--- JS/script.js
+++ JS/script.js
@@ -8,7 +8,7 @@ const handleInfiniteScroll = () => {
   const endOfPage =
     document.documentElement.scrollTop +
       document.documentElement.clientHeight >=
-    document.documentElement.scrollHeight - 5;
+    document.documentElement.scrollHeight - 10;
   if (endOfPage && currentpage < lastpage) {
     getposts(currentpage++);
   }

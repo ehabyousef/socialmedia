012943effa0cb332f3d048eb046009038b77399a
‪Ehab Yousef‬‏ <ehabyousef022@gmail.com>
2023-09-09 18:33:23 +0300

    add functions

--- JS/profile.js
+++ JS/profile.js
@@ -1,7 +1,32 @@
 let postCard = document.getElementById("post");
 const baseurl = "https://tarmeezacademy.com/api/v1/";
 
+// function for butons
+function showLogBtn() {
+  let loginBtn = document.getElementById("loginBtn");
+  let logoutBtn = document.getElementById("logoutBtn");
+  let addPost = document.getElementById("addPost");
+  let profilename = document.getElementById("profile-name");
+  let profileimage = document.getElementById("profile-image");
 
-
-
-// function for butons 
+  let taken = localStorage.getItem("token");
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (taken == null) {
+    logoutBtn.classList.add("BTN-Hide");
+    loginBtn.classList.remove("BTN-Hide");
+    addPost.classList.add("BTN-Hide");
+  } else {
+    logoutBtn.classList.remove("BTN-Hide");
+    loginBtn.classList.add("BTN-Hide");
+    addPost.classList.remove("BTN-Hide");
+    profilename.innerHTML = user.name;
+    profileimage.src = user.profile_image;
+  }
+}
+showLogBtn();
+function logOutStorage() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+  showLogBtn();
+  location.reload();
+}
